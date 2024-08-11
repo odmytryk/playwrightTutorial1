0f288d05870f6454bc5b8c28e9048fe589c65d83
odmytryk <olha.dmytryk@traxretail.com>
2024-08-11 09:17:54 +0200

    Delete same tests with 'Shop now' button

--- test_ua_laout/test_text_is_present_on_home_page.py
+++ test_ua_laout/test_text_is_present_on_home_page.py
@@ -2,8 +2,8 @@ from playwright.sync_api import Playwright, sync_playwright, expect
 from test_ua_laout.home_page_elements import TtestHomePage
 import pytest
 
-def test_celebrate_header_is_present(log_in) -> None:
-    page = log_in
+def test_celebrate_header_is_present(set_up) -> None:
+    page = set_up
     home_page = TtestHomePage(page)
     expect(home_page.celebrate_header).to_contain_text("Celebrating")
 
@@ -20,24 +20,3 @@ def test_shop_now_button_is_present(setup_log_in) -> None:
     page = setup_log_in
     expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
 
-@pytest.mark.integration
-@pytest.mark.smoke
-def test_shop_now_button_is_present_2(setup_log_in) -> None:
-    page = setup_log_in
-    expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
-
-
-@pytest.mark.smoke
-def test_shop_now_button_is_present_3(setup_log_in) -> None:
-    page = setup_log_in
-    expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
-
-def test_shop_now_button_is_present_4(setup_log_in) -> None:
-    page = setup_log_in
-    expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
-
-
-@pytest.mark.smoke
-def test_shop_now_button_is_present_5(setup_log_in) -> None:
-    page = setup_log_in
-    expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
