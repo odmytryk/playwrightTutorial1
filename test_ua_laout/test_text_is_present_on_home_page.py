785b563040fa4726e579a54a5b4355026e97e884
odmytryk <34870578+odmytryk@users.noreply.github.com>
2024-08-12 08:43:21 +0200

    Update test_text_is_present_on_home_page.py

--- test_ua_laout/test_text_is_present_on_home_page.py
+++ test_ua_laout/test_text_is_present_on_home_page.py
@@ -1,22 +1,22 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
-from test_ua_laout.home_page_elements import TtestHomePage
-import pytest
+# from playwright.sync_api import Playwright, sync_playwright, expect
+# from test_ua_laout.home_page_elements import TtestHomePage
+# import pytest
 
-def test_celebrate_header_is_present(set_up) -> None:
-    page = set_up
-    home_page = TtestHomePage(page)
-    expect(home_page.celebrate_header).to_contain_text("Celebrating")
+# def test_celebrate_header_is_present(set_up) -> None:
+#     page = set_up
+#     home_page = TtestHomePage(page)
+#     expect(home_page.celebrate_header).to_contain_text("Celebrating")
 
 
-@pytest.mark.skip(reason="not reeaadyy daddy")
-def test_celebrate_body_is_present(set_up) -> None:
-    page = set_up
-    home_page = TtestHomePage(page)
-    expect(home_page.celebrate_body).to_be_visible()
-    expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
+# @pytest.mark.skip(reason="not reeaadyy daddy")
+# def test_celebrate_body_is_present(set_up) -> None:
+#     page = set_up
+#     home_page = TtestHomePage(page)
+#     expect(home_page.celebrate_body).to_be_visible()
+#     expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
 
-#@pytest.mark.xfail(reason="development is still not ready")
-def test_shop_now_button_is_present(setup_log_in) -> None:
-    page = setup_log_in
-    expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
+# #@pytest.mark.xfail(reason="development is still not ready")
+# def test_shop_now_button_is_present(setup_log_in) -> None:
+#     page = setup_log_in
+#     expect(page.get_by_role("button", name="Shop Now")).to_be_visible()
 
